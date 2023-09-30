b352f91bd775950cd3d2829a88007d07c5273eb2
James Home <james@jameshome.co.uk>
2023-10-01 00:53:07 +0100

    Clean up

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,7 @@ import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 export default async function Home({ params }) {
   const client = createClient();
-  console.log(client)
   const page = await client.getByUID("home", "home");
-  console.log(page)
-  console.log(components)
   return (
     <main>
       <SliceZone

f6dcd074756a98a24240ccbb98ac376d83b7e9c1
James Home <james@jameshome.co.uk>
2023-10-02 03:42:49 +0100

    Conform buttons

--- src/slices/Form/index.js
+++ src/slices/Form/index.js
@@ -152,7 +152,7 @@ const VariationDefault = ({ primary, items }) => {
       <div className="form-item mt-6">
         <button
           type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="button-primary button px-6"
         >
           {children}
         </button>

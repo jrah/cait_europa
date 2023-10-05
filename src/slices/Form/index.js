89e5d64e8bd0799e136bdc98cb873de30dc4e7eb
James Home <james@jameshome.co.uk>
2023-10-05 02:49:07 +0100

    Horizontal spacing textarea

--- src/slices/Form/index.js
+++ src/slices/Form/index.js
@@ -151,7 +151,7 @@ const VariationDefault = ({ primary, items }) => {
             id={label}
             name={label}
             rows={3}
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             defaultValue={''}
           /></div>
       </div>

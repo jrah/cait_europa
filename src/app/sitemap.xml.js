61765c0afcc6d77ac3922447efddd087a5d78e21
James Home <james@jameshome.co.uk>
2023-10-24 01:40:28 +0100

    Add sitemap

---
+++ src/app/sitemap.xml.js
@@ -0,0 +1,18 @@
+import { CreateClient } from "@prismicio/client"
+
+export default async function sitemap() {
+    const url = `http://${process.env.BASE_URL}`
+    const client = createClient();
+    const getAllPages = await client.getAllByType("pages");
+    const pages = getAllPages.map(({ id, date }) => ({
+        url: `${url}/${id}`,
+        lastModified: date.toUTCString
+    }))
+
+    const routes = [""].map((route) => ({
+        url: `${URL}${route}`,
+        lastModified: new Date().toISOString(),
+    }));
+
+    return [...routes, ...pages]
+}

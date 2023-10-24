9da348eb748968b8faab7773b7ff6aff225bf978
James Home <james@jameshome.co.uk>
2023-10-24 03:53:59 +0100

    Correct typos

--- src/slices/TextSplash/variants/TextSplashWithBody/index.jsx
+++ src/slices/TextSplash/variants/TextSplashWithBody/index.jsx
@@ -44,7 +44,7 @@ const VariationTextSplashWithBody = ({ primary, items }) => {
                             listItem: ({ children }) => (
                                 <li className="flex gap-x-3">
                                     <svg className="h-7 w-5 flex-none" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd"></path>
+                                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" ClipRule="evenodd"></path>
                                     </svg>{children}
                                 </li>
                             ),
